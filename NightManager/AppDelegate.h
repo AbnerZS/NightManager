a36ebe87f496d30530b6f47931c62e3eec335367
AbnerZS <abnerzhang_good@icloud.com>
2016-12-26 13:24:05 +0800

    Initial Commit

---
+++ NightManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NightManager
+//
+//  Created by abnerzhang on 2016/12/26.
+//  Copyright © 2016年 abnerzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
